0cb964062068a87d40ce56a8cc388fbebb29531b
周昊 <zhouhao1@corp.netease.com>
2018-05-04 16:02:18 +0800

    入门

---
+++ src/helper.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zh on 2018/5/3.
+ */
+function sum(a, b) {
+  return a + b
+}
+
+function minus(a, b) {
+  return a - b
+}
+
+export {sum, minus} ;
---
+++ test/unit/helper.test.js
@@ -0,0 +1,84 @@
+/**
+ * Created by zh on 2018/5/3.
+ */
+import {sum, minus} from '@/helper'
+
+describe('helper test', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('minus 5 - 2 to equal 3', () => {
+    expect(minus(5, 2)).toBe(3);
+  });
+
+  test('object', () => {
+    let obj = {value: 'a'};
+    expect(obj.value).toEqual('a')
+  });
+
+  test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
+
+  test('this is a joe in the sentence?', () => {
+    expect('this is joe').toMatch('joe')
+  });
+
+  test('the Array contains the item?', () => {
+    const arr = ['a', 'n', 'cc'];
+    expect(arr).toContain('cc')
+  });
+
+  function fn() {
+    throw new Error('this is a error')
+  }
+
+  test('function will throw an error', () => {
+    expect(fn).toThrow();
+    expect(fn).toThrow(Error);
+
+    expect(fn).toThrow('this is a error');
+    expect(fn).toThrow(/is/);
+  });
+
+  function fetchDate() {
+    return new Promise((resolve, reject) => {
+      setTimeout(reject, 3000, 'ok')
+    })
+  }
+
+  test('the data is ok', () => {
+    expect.assertions(1);
+    return fetchDate().catch(val => {
+      expect(val).toBe('ok')
+    })
+  });
+
+  function asyncFetch() {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, 3000, 'fine')
+    })
+  }
+
+  test('async function to be tested', async () => {
+    expect.assertions(1);
+    const result = await asyncFetch();
+    expect(result).toBe('fine')
+  });
+
+  const filterFn = jest.fn();
+  filterFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+  const result = [11, 22].filter(filterFn);
+  test('mock value once', ()=>{
+    expect(result).toEqual([11]);
+    expect(result).not.toContain(22)
+  });
+
+  const mockFn = jest.fn().mockImplementation(val =>  val +100)
+  console.log(mockFn(100))
+});
